c48d27de1e4d007492abab5234425b81474d083b
Emerson Macedo <56753061+emersonmacedo123@users.noreply.github.com>
2025-11-20 22:04:35 +0100

    Firstcommit helloworld

---
+++ app.py
@@ -0,0 +1,12 @@
+from flast import Flask
+
+app = Flast(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, welcome to WordMemorizer!"
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
